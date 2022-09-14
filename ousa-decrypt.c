303fa617b13162e02d71dbf33ee4d19975df31b4
dodo-sec <110191752+dodo-sec@users.noreply.github.com>
2022-09-14 14:14:39 +0000

    Updated optional flag checking
    Changed optional flag check to a while loop that does not rely on a fixed argv position. Pointers for input and output file opening also do not rely on fixed argv positions anymore, which eliminates the need for an if/else block and the opt_flag variable.

--- ousa-decrypt.c
+++ ousa-decrypt.c
@@ -3,57 +3,42 @@
 int main(int argc, char *argv[]) {
 	FILE* ptr;
 	FILE* dec;
-	int index, input_c, output_c, magic, key;
-	int opt_flag = 0;
-
-	//check for optional flag
-	if (argv[1][0] == '-') {
-		switch (argv[1][1]) {
-		case 'm':	//specifies value for key generation
-			opt_flag = 1;
-			break;
-		case 'h':	//prints usage
-			argc = 0;
-			break;
-		default:
-			printf("illegal option %c\n", argv[1][1]);
-			break;
-		}
-	}
+	int c, index, input_c, output_c, key;
+	int magic = 0x161a;
 	
-	if (argc < 3 || argc > 5) {
+	//check for presence of optional flags;
+	while (--argc > 0 && (*++argv)[0] == '-')
+		while (c = *++argv[0])
+			switch (c)
+			{
+			case 'h':
+				argc = 0;
+				break;
+			case 'm':
+				magic = strtol(*++argv, NULL, 16);	//converts user input magic to int hex;
+				printf("Using magic %x\n", magic);
+				break;
+			default:
+				break;
+			}
+	
+	if (argc != 3) {	//checks for correct amount of remaining arguments after counting optional flags;
 		printf("%s", "Usage: ousa-decrypt {optional flag} [ENCRYPTED FILE] [OUTPUT FILE]\n\n");
 		printf("%s\t%s", "-h", "show this help menu\n");
 		printf("%s\t%s", "-m", "magic value for key generation (default is 0x161a)\n");
 		return;
 	}
 
-	if (opt_flag > 0) {
-		magic = strtol(argv[2], NULL, 16);	//assign magic from user input
-		ptr = fopen(argv[3], "rb");
-		if (ptr == NULL) {
-			printf("%s", "Can't open input file\n");
-			return;
-		}
-		dec = fopen(argv[4], "wb");
-		if (dec == NULL) {
-			printf("%s", "Can't open output file\n");
-			return;
-		}
+	ptr = fopen(*argv++, "rb");	//increments argv after use to point to output file for next open operation;
+	if (ptr == NULL) {
+		printf("%s", "Can't open input file\n");
+		return;
 	}
-
-	else {
-		magic = 0x161a;
-		ptr = fopen(argv[1], "rb");
-		if (ptr == NULL) {
-			printf("%s", "Can't open input file\n");
-			return;
-		}
-		dec = fopen(argv[2], "wb");
-		if (dec == NULL) {
-			printf("%s", "Can't open output file\n");
-			return;
-		}
+	
+	dec = fopen(*argv, "wb");
+	if (dec == NULL) {
+		printf("%s", "Can't open output file\n");
+		return;
 	}
 
 	for (index = 0; (input_c = fgetc(ptr)) != EOF; ++index) {
@@ -62,6 +47,7 @@ int main(int argc, char *argv[]) {
 		output_c = input_c ^ key;
 		fputc(output_c, dec);
 	}
+	printf("%s", "Done writing output file\n");
 	fclose(ptr);
 	fclose(dec);
 
